e760b36959f2cc69f757d0c31d374ca8355c21cc
xezon <4720891+xezon@users.noreply.github.com>
2026-07-26 12:13:27 +0200

    chore(string): Implement AsciiString::toUpper() (#3020)

--- Core/GameEngine/Source/Common/System/AsciiString.cpp
+++ Core/GameEngine/Source/Common/System/AsciiString.cpp
@@ -427,6 +427,31 @@ void AsciiString::toLower()
 }
 
 // -----------------------------------------------------
+void AsciiString::toUpper()
+{
+	validate();
+
+	if (m_data == nullptr)
+		return;
+
+	const StringCaseInfo info = getStringCaseInfo(m_data->peek());
+
+	if (info.lowercaseCount == 0)
+		return;
+
+	ensureUniqueBufferOfSize(info.length + 1, true, nullptr, nullptr);
+
+	char* str = m_data->peek();
+	while (*str)
+	{
+		*str = toupper((unsigned char)*str);
+		++str;
+	}
+
+	validate();
+}
+
+// -----------------------------------------------------
 void AsciiString::removeLastChar()
 {
 	truncateBy(1);
