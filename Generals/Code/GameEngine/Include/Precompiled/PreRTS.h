08875d2aec69d3065a073c587930762ea49e4451
Jaredl-Dev <260281143+Jaredl-Dev@users.noreply.github.com>
2026-08-02 08:14:41 -0700

    unify(precompiled): Merge Precompiled from Zero Hour (#3050)

--- Generals/Code/GameEngine/Include/Precompiled/PreRTS.h
+++ Generals/Code/GameEngine/Include/Precompiled/PreRTS.h
@@ -124,6 +124,7 @@ class STLSpecialAlloc;
 #include "Common/NameKeyGenerator.h"
 #include "GameClient/ClientRandomValue.h"
 #include "GameLogic/LogicRandomValue.h"
+#include "Common/ObjectStatusTypes.h"
 
 #include "Common/Thing.h"
 #include "Common/UnicodeString.h"
