912f26fd1dfc4b1c77846915fed365808d98c930
x64-dev <202863051+x64-dev@users.noreply.github.com>
2026-08-03 17:44:53 -0400

    - Load community patch + apply other recommended flags

--- Core/GameEngine/Source/Common/System/ArchiveFileSystem.cpp
+++ Core/GameEngine/Source/Common/System/ArchiveFileSystem.cpp
@@ -50,6 +50,7 @@
 #include "Common/ArchiveFileSystem.h"
 #include "Common/AsciiString.h"
 #include "Common/PerfTimer.h"
+#include "../NGMP_include.h"
 
 
 //----------------------------------------------------------------------------
@@ -212,6 +213,13 @@ void ArchiveFileSystem::loadIntoDirectoryTree(ArchiveFile *archiveFile, Bool ove
 
 void ArchiveFileSystem::loadMods()
 {
+#if defined(GENERALS_ONLINE) && defined(GENERALS_ONLINE_COMMUNITY_PATCH_CHANGES)
+    // load community data patch BIG
+    std::string strSettingsFileDir = std::format("{}/GeneralsOnlineGameData/", TheGlobalData->getPath_UserData().str());
+    bool bLoaded = TheArchiveFileSystem->loadBigFilesFromDirectory(strSettingsFileDir.c_str(), "500_900_CommunityPatch_CoreINI_v6.big", TRUE);
+    NetworkLog(ELogVerbosity::LOG_RELEASE, "Loaded community patch: %d", bLoaded);
+#endif
+
 	if (TheGlobalData->m_modBIG.isNotEmpty())
 	{
 		ArchiveFile *archiveFile = openArchiveFile(TheGlobalData->m_modBIG.str());
