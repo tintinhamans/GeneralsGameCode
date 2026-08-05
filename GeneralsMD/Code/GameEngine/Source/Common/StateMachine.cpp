9262f695d56328e4128799261fd2ec717497d57d
xezon <4720891+xezon@users.noreply.github.com>
2026-08-05 21:44:02 +0200

    refactor(refcountptr): Simplify RefCountPtr's Create_No_Add_Ref, Create_Add_Ref and add Assign_No_Add_Ref, Assign_Add_Ref (#3058)

--- GeneralsMD/Code/GameEngine/Source/Common/StateMachine.cpp
+++ GeneralsMD/Code/GameEngine/Source/Common/StateMachine.cpp
@@ -433,7 +433,7 @@ StateReturnType StateMachine::updateStateMachine()
 		// Calling m_currentState->update() can release this state machine in certain circumstances,
 		// for example if something kills the entity of this state machine as a result of this state update.
 		// See https://github.com/TheSuperHackers/GeneralsGameCode/issues/212
-		RefCountPtr<StateMachine> refThis = RefCountPtr<StateMachine>::Create_AddRef(this);
+		RefCountPtr<StateMachine> refThis = Create_Add_Ref(this);
 
 		// update() can change m_currentState, so save it for a moment...
 		State* stateBeforeUpdate = m_currentState;
