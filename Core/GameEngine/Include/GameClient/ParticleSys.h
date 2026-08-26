934bfa932658ee312691185fa1ba76b8200ad274
Bobby Battista <bobtista@gmail.com>
2026-08-26 12:44:59 -0500

    feat(saveload): Implement -loadsave command line option (#3001)

--- Core/GameEngine/Include/GameClient/ParticleSys.h
+++ Core/GameEngine/Include/GameClient/ParticleSys.h
@@ -862,6 +862,8 @@ public:
 
 	virtual Bool isDummy() const override { return true; }
 
+	virtual Bool isXferEnabled() const override { return FALSE; }
+
 	virtual Int getOnScreenParticleCount() override { return 0; }
 	virtual void doParticles(RenderInfoClass &rinfo) override {}
 	virtual void queueParticleRender() override {}
