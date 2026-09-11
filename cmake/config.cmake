afd41574d16b77cacee80755a64fd9360a5ba51e
tintinhamans <5984296+tintinhamans@users.noreply.github.com>
2026-09-11 12:47:48 +0200

    build(cmake): Add retail game compatibility option

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -1,5 +1,6 @@
 add_library(core_config INTERFACE)
 
 include(${CMAKE_CURRENT_LIST_DIR}/config-build.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/config-retail.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/config-debug.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/config-memory.cmake)
