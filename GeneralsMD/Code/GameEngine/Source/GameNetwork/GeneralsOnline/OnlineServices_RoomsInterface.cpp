f23b1ccf0e8b73fee7ba7fb8841b05e1942a9e56
tintinhamans <5984296+tintinhamans@users.noreply.github.com>
2026-09-23 23:31:46 +0200

    fix(http): Verify TLS against the OS certificate store

--- GeneralsMD/Code/GameEngine/Source/GameNetwork/GeneralsOnline/OnlineServices_RoomsInterface.cpp
+++ GeneralsMD/Code/GameEngine/Source/GameNetwork/GeneralsOnline/OnlineServices_RoomsInterface.cpp
@@ -197,29 +197,10 @@ void WebSocket::Connect(const char* url, bool bIsReconnect, std::function<void(v
 
 		curl_easy_setopt(m_pCurlWS, CURLOPT_VERBOSE, 1L);
 #else
-        if (HTTPManager::IsCACertStoreBad())
-        {
-            curl_easy_setopt(m_pCurlWS, CURLOPT_SSL_VERIFYPEER, 0);
-            curl_easy_setopt(m_pCurlWS, CURLOPT_SSL_VERIFYHOST, 0);
-        }
-        else
-        {
-            std::ifstream certFile("cacert.pem");
-            if (certFile.good())
-            {
-                certFile.close();
-                curl_easy_setopt(m_pCurlWS, CURLOPT_CAINFO, "cacert.pem");
-
-                curl_easy_setopt(m_pCurlWS, CURLOPT_SSL_VERIFYPEER, 1L);
-                curl_easy_setopt(m_pCurlWS, CURLOPT_SSL_VERIFYHOST, 2L);
-            }
-            else
-            {
-				HTTPManager::SetCACertStoreBad();
-                curl_easy_setopt(m_pCurlWS, CURLOPT_SSL_VERIFYPEER, 0);
-                curl_easy_setopt(m_pCurlWS, CURLOPT_SSL_VERIFYHOST, 0);
-            }
-        }
+        // Use the OS cert store (needed since we build curl against OpenSSL, not schannel).
+        curl_easy_setopt(m_pCurlWS, CURLOPT_SSL_OPTIONS, CURLSSLOPT_NATIVE_CA);
+        curl_easy_setopt(m_pCurlWS, CURLOPT_SSL_VERIFYPEER, 1L);
+        curl_easy_setopt(m_pCurlWS, CURLOPT_SSL_VERIFYHOST, 2L);
 #endif
 
 
