5486cec7cd28682fa9597f60e92fa5449cc8c247
tintinhamans <5984296+tintinhamans@users.noreply.github.com>
2026-09-24 04:35:15 +0200

    fix(draw): Guard missing tint envelope in Overlord tank and truck draw

--- Core/GameEngineDevice/Source/W3DDevice/GameClient/Drawable/Draw/W3DOverlordTruckDraw.cpp
+++ Core/GameEngineDevice/Source/W3DDevice/GameClient/Drawable/Draw/W3DOverlordTruckDraw.cpp
@@ -86,7 +86,9 @@ void W3DOverlordTruckDraw::doDrawModule(const Matrix3D* transformMtx)
 		)
 	{
 		Drawable *riderDraw = me->getContain()->friend_getRider()->getDrawable();
-		riderDraw->setColorTintEnvelope( *getDrawable()->getColorTintEnvelope() );
+		TintEnvelope *env = getDrawable()->getColorTintEnvelope();
+		if ( env )
+			riderDraw->setColorTintEnvelope( *env );
 
 		riderDraw->notifyDrawableDependencyCleared();
 		riderDraw->draw();
